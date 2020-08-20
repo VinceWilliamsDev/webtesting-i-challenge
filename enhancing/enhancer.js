97e672b8d983a6f5173da6f37495435cbca47f30
Vince Williams <williamsvm20@gmail.com>
2020-08-20 17:41:21 -0400

    tests are finished and are passing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,38 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement >= 20) return item
+  return { 
+    ...item, 
+    enhancement: (item.enhancement + 1)
+  }; 
 }
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement >= 15 && item.durability < 10 || item.enhancement < 15 && item.durability < 5) {
+    return item
+  }
+  if (item.enhancement > 16) return {
+    ...item, 
+    durability: (item.durability -10), 
+    enhancement: (item.enhancement -1)
+  }
+  if (item.enhancement < 15) return {
+    ...item,
+    durability: (item.durability - 5)
+  }
+  return {
+    ...item,
+    durability: (item.durability - 10)
+  }
+} 
+
 
 function repair(item) {
-  return { ...item };
+  return { 
+    ...item, 
+    durability: 100
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,109 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const {
+    success,
+    fail,
+    repair,
+    get,
+  } = enhancer
+
+describe("test the enhacer module", () => {
+    describe("test the repair method", () => {
+        it("accepts an item obj and returns the item with 100 durability", () => {
+            const item = {
+                durability: 30,
+                enhancement: 5,
+                name: 'Sword'
+            }
+
+            const itemTwo = {
+                durability: 100,
+                enhancement: 5,
+                name: 'Sword'
+            }
+
+            const repairedItem = repair(item)
+            expect(repairedItem.durability).toBe(100)
+            expect(repairedItem).toEqual(itemTwo)
+        })
+    })
+    describe("testing the success method", () => {
+        it("returns the item with the enhacement level increased by 1", () => {
+            const item = {
+                durability: 30,
+                enhancement: 5,
+                name: 'Sword'
+            }
+
+            const itemTwo = {
+                durability: 30,
+                enhancement: 6,
+                name: 'Sword'
+            }
+
+            const enhancedItem = success(item)
+            expect(enhancedItem).toEqual(itemTwo)
+        })
+        it("doesn't enhance the item if the enhancement level is 20 already", () => {
+            const item = {
+                durability: 30,
+                enhancement: 20,
+                name: 'Sword'
+            }
+
+            const enhancedItem = success(item)
+            expect(enhancedItem).toEqual(item)
+        })
+    })
+    describe("testing the fail module", () => {
+        it("the item's durability is reduced by 5 if the enhancement level is less than 15", () => {
+            const item = {
+                durability: 30,
+                enhancement: 14,
+                name: 'Sword'
+            }
+
+            const itemTwo = {
+                durability: 25,
+                enhancement: 14,
+                name: 'Sword'
+            }
+
+            const enhancedItem = fail(item)
+            expect(enhancedItem).toEqual(itemTwo)
+        })
+        it("the item's durability is reduced by 10 if the enhancement level is 15 or more", () => {
+            const item = {
+                durability: 30,
+                enhancement: 15,
+                name: 'Sword'
+            }
+
+            const itemTwo = {
+                durability: 20,
+                enhancement: 15,
+                name: 'Sword'
+            }
+
+            const enhancedItem = fail(item)
+            expect(enhancedItem).toEqual(itemTwo)
+        })
+        it("the item's durability is reduced by 10 if the enhancement level is 15 or more", () => {
+            const item = {
+                durability: 30,
+                enhancement: 17,
+                name: 'Sword'
+            }
+
+            const itemTwo = {
+                durability: 20,
+                enhancement: 16,
+                name: 'Sword'
+            }
+
+            const enhancedItem = fail(item)
+            expect(enhancedItem).toEqual(itemTwo)
+        })
+    })
+})
